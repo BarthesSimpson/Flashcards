244dfeb27f39d7ed7bc36b6c19b553833862d866
BarthesSimpson <d.s.frenk@gmail.com>
2017-09-21 22:29:05 -0500

    added styled-components and react-navigation

--- App.js
+++ App.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react"
+import { StyleSheet, Text, View } from "react-native"
+import styled from "styled-components/native"
+import { TabNavigator } from "react-navigation"
+
+const StyledView = styled.View`background-color: papayawhip;`
+
+const Hello = () => (
+  <StyledView>
+    <Text>Hello!</Text>
+  </StyledView>
+)
+
+const Goodbye = () => (
+  <StyledView>
+    <Text>Goodbye!</Text>
+  </StyledView>
+)
+
+const Tabs = TabNavigator({
+  Hello: {
+    screen: Hello
+  },
+  Goodbye: {
+    screen: Goodbye
+  }
+})
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <Tabs />
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
