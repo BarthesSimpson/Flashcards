8c681896870d12833a76863339b398153e5a0cd3
BarthesSimpson <d.s.frenk@gmail.com>
2017-09-21 22:56:34 -0500

    set up some initial navigation

--- App.js
+++ App.js
@@ -1,18 +1,39 @@
 import React from "react"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, StatusBar } from "react-native"
+import { Button } from "react-native-elements"
 import styled from "styled-components/native"
 import { TabNavigator } from "react-navigation"
+import { Constants } from "expo"
 
-const StyledView = styled.View`background-color: papayawhip;`
+const StyledView = styled.View`
+  background-color: papayawhip;
+  flex: 1;
+`
+const StyledStatusBar = styled.StatusBar`background-color: blue;`
+
+function NiceStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ height: Constants.statusBarHeight }}>
+      <StyledStatusBar translucent />
+    </View>
+  )
+}
 
 const Hello = () => (
   <StyledView>
-    <Text>Hello!</Text>
+    <NiceStatusBar />
+    <Button
+      large
+      icon={{ name: "squirrel", type: "octicon" }}
+      title="HELLO"
+      buttonStyle={{ backgroundColor: "blue" }}
+    />
   </StyledView>
 )
 
 const Goodbye = () => (
   <StyledView>
+    <NiceStatusBar />
     <Text>Goodbye!</Text>
   </StyledView>
 )
@@ -28,7 +49,12 @@ const Tabs = TabNavigator({
 
 export default class App extends React.Component {
   render() {
-    return <Tabs />
+    return (
+      <View style={{ flex: 1 }}>
+        <StyledStatusBar />
+        <Tabs />
+      </View>
+    )
   }
 }
 
