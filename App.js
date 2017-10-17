aa6c20bd1490ffb319cffe0e62d015cd830a27de
BarthesSimpson <d.s.frenk@gmail.com>
2017-10-16 20:03:34 -0500

    fixed typo

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import LandingScreen from './src/components/LandingScreen'
 //REDUX
 import { store } from './src/ducks'
 import { Provider } from 'react-redux'
-import { setLocalNotification } from '.src/ducks/notifications'
+import { setLocalNotification } from './src/ducks/notifications'
 
 export default class App extends React.Component {
   componentDidMount() {
