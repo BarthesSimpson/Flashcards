12f1e9a7f0df7c6b32a52113126e1351e7f9aa65
BarthesSimpson <d.s.frenk@gmail.com>
2017-10-16 20:09:36 -0500

    only enable notifications if user has completed one quiz

--- App.js
+++ App.js
@@ -5,12 +5,8 @@ import LandingScreen from './src/components/LandingScreen'
 //REDUX
 import { store } from './src/ducks'
 import { Provider } from 'react-redux'
-import { setLocalNotification } from './src/ducks/notifications'
 
 export default class App extends React.Component {
-  componentDidMount() {
-    setLocalNotification()
-  }
   render() {
     return (
       <Provider store={store}>
